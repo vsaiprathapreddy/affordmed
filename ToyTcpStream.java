b1810959331368ef5e1699af4ca20aa35df92c65
vsaiprathapreddy <saiprathap3424@gmail.com>
2021-10-12 19:58:14 +0530

    tried tcpstream

---
+++ ToyTcpStream.java
@@ -0,0 +1,12 @@
+class ToyTcpStream{
+    public void receive(int chunk,byte[] data){
+        char receive[]=new char[data.length]
+        if (chunk = 0){
+
+        }
+    }
+    public int read([]byte data)
+    public static void main(String [] args){
+
+    }
+}
